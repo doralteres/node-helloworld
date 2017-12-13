8a035a949fbd262da88a8f910076eb2d1290a962
yahav <yahav@emergency2-tlv.lpnet.com>
2017-12-13 17:22:00 +0200

    params

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,17 @@ module.exports = function(grunt) {
 	watch: {
             files: ['Gruntfile.js'],
             tasks: ['jshint']
-    	}
+    	},
+	release: {
+    	    options: {
+         	github: {
+        		repo: 'doralteres/node-helloworld',
+        		usernameVar: 'GITHUB_USERNAME', //ENVIRONMENT VARIABLE that contains GitHub username
+        		passwordVar: 'GITHUB_PASSWORD' //ENVIRONMENT VARIABLE that contains GitHub password
+      		}
+    	    }
+  }
     });
     grunt.registerTask('default', ['jshint']);
+    grunt.loadNpmTasks('grunt-release');
 };
